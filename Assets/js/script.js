3ad9b32409cb12977883cfd138aea3f888c3229d
Jeanette Ralph <jean.ralph@hotmail.com>
2022-06-02 14:28:49 -0700

    second commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,66 +1,90 @@
+$(document).ready(function () {
+
 // defining variables globally
 var currentDate = moment().format("dddd, MMMM Do YYYY");
-var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-var rowTimeRef = moment().format("HH:mm:ss a");
+var rowTimeRef = moment().hour();
 var saveBtn = $('#saveBtn');
+var rowTime = $('.hour');
 
 // current time being displayed in header
-$(function currentDate() {
+function currentDate() {
     var currentDate = moment().format("dddd, MMMM Do YYYY");
-    $("#currentDay").text(currentDate);
+    $("#current-day").text(currentDate);
     console.log(currentDate);
-});
+};
 
 // function to check if a row time block area is in the past/ present or future
-$(function timeChecker() {
-    // need a reference variable to the current time
-    // made it 24 hr time so it is easy to compare
-    var rowTimeRef = moment().format("HH:mm:ss A");
-    // need to create a var that will trach the row id numbers
+function timeChecker() {
 
-    // create an if statement to check if time is past 
-        // call the past style to display
-    if ( rowTime < rowTimeRef) {
-        eventRow == 'past';
-    }
+    // using a for each loop to grab each id for the hour block
+    rowTime.each(function() {
 
-    // create an if statement to check if time is present
-        // call the present style to display
-    if (rowTime = rowTimeRef) {
-        eventRow == 'present';
-    }
+        
+        // getting the value of the unique id for the time
+        var hourTime = $(this).attr('id');
+        var hourNumber = parseInt(hourTime, 10);
 
-    // create an if statement to check if time is future
-        // call the future style to display
-    if (rowTime = rowTimeRef) {
-        eventRow == 'future';
-    }
+        // getting the text area that will change color
+        var textBlock = $(this).siblings('.note-box');
 
-});
+        // checking if hour number is less than our referance var
+        // adding the past class
+        if ( hourNumber < rowTimeRef) {
+            textBlock.addClass('past');
+        }
+     
+        // checking if hour number is equal to our referance var
+        // removing the past class and adding the present class
+        if (hourNumber === rowTimeRef) {
+            textBlock.removeClass('past');
+            textBlock.addClass('present');
+        }
+    
+        // checking if hour number is greater than our referance var
+        // removing the other styles and adding the future style
+        if (hourNumber > rowTimeRef) {
+            textBlock.removeClass('past');
+            textBlock.removeClass('present');
+            textBlock.addClass('future');
+        }
+    })
+};
 
-// saving to local storage function
-saveBtn.on('click', function()
+timeChecker();
+currentDate();
+
+
+// saving to local storage function + keeping input on page
+// for each loop like above
+// update save button to select class
+// get text input same as button
+// select the one that matches
+
+saveBtn.on('click', function savingInput()
 {
-    // saving the value to local storage 
-    var eventInput = $('#event');
-    localStorage.setItem('eventToDo', eventInput.val());
+    // added form-control class to the save button in index
 
-    // calling it by eventToDo
-    var storedEvent = localStorage.getItem("eventToDo");
-    return storedEvent;
-}
-);
+    // selecting the class on the input tag
+    var inputs = $(".form-control");
 
-// making sure thee input stays on the page after it is refreshed
+    // added the same time id to the button and the form, but not the button div
 
-$(function pageRefresh() {
-    document.getElementById('event').innerHTML = localStorage.getItem('eventToDo');
+    // doing a for each loop
+    inputs.each(function () {
+        var value = $(this).val();
+        var time = $(this).attr('id');
+        var storedEvent = localStorage.setItem(time, value);
 
+        var finalEvent = localStorage.getItem(storedEvent);
+        return finalEvent;
+        
+    })
+
+    // calling the function to load 
+    window.onload() = savingInput;
+    
+});
 });
 
-window.onload = pageRefresh; 
+savingInput();
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I refresh the page
-// THEN the saved events persist 
