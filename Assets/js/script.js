ca2be0cdcbf929d4d0a7e191a7c0f790ec12a7a4
Jeanette Ralph <jean.ralph@hotmail.com>
2022-06-02 21:28:32 -0700

    fixing branch

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,90 +1,111 @@
 $(document).ready(function () {
 
-// defining variables globally
-var currentDate = moment().format("dddd, MMMM Do YYYY");
-var rowTimeRef = moment().hour();
-var saveBtn = $('#saveBtn');
-var rowTime = $('.hour');
-
-// current time being displayed in header
-function currentDate() {
+    // defining variables globally
     var currentDate = moment().format("dddd, MMMM Do YYYY");
-    $("#current-day").text(currentDate);
-    console.log(currentDate);
-};
+    var rowTimeRef = moment().hour();
+    var saveBtn = $('#saveBtn');
+    var rowTime = $('.hour');
 
-// function to check if a row time block area is in the past/ present or future
-function timeChecker() {
-
-    // using a for each loop to grab each id for the hour block
-    rowTime.each(function() {
-
-        
-        // getting the value of the unique id for the time
-        var hourTime = $(this).attr('id');
-        var hourNumber = parseInt(hourTime, 10);
-
-        // getting the text area that will change color
-        var textBlock = $(this).siblings('.note-box');
-
-        // checking if hour number is less than our referance var
-        // adding the past class
-        if ( hourNumber < rowTimeRef) {
-            textBlock.addClass('past');
-        }
-     
-        // checking if hour number is equal to our referance var
-        // removing the past class and adding the present class
-        if (hourNumber === rowTimeRef) {
-            textBlock.removeClass('past');
-            textBlock.addClass('present');
-        }
-    
-        // checking if hour number is greater than our referance var
-        // removing the other styles and adding the future style
-        if (hourNumber > rowTimeRef) {
-            textBlock.removeClass('past');
-            textBlock.removeClass('present');
-            textBlock.addClass('future');
-        }
-    })
-};
-
-timeChecker();
-currentDate();
+    // current time being displayed in header
+    function displayDate() {
+        var currentDate = moment().format("dddd, MMMM Do YYYY");
+        $("#current-day").text(currentDate);
+        console.log(currentDate);
+    };
 
 
-// saving to local storage function + keeping input on page
-// for each loop like above
-// update save button to select class
-// get text input same as button
-// select the one that matches
+    // function to check if a row time block area is in the past/ present or future
+    function timeChecker() {
 
-saveBtn.on('click', function savingInput()
-{
-    // added form-control class to the save button in index
+        // using a for each loop to grab each id for the hour block
+        rowTime.each(function () {
 
-    // selecting the class on the input tag
-    var inputs = $(".form-control");
 
-    // added the same time id to the button and the form, but not the button div
+            // getting the value of the unique id for the time
+            var hourTime = $(this).attr('id');
+            var hourNumber = parseInt(hourTime, 10);
 
-    // doing a for each loop
-    inputs.each(function () {
-        var value = $(this).val();
-        var time = $(this).attr('id');
-        var storedEvent = localStorage.setItem(time, value);
+            // getting the text area that will change color
+            var textBlock = $(this).siblings('.note-box');
 
-        var finalEvent = localStorage.getItem(storedEvent);
-        return finalEvent;
-        
-    })
+            // checking if hour number is less than our referance var
+            // adding the past class
+            if (hourNumber < rowTimeRef) {
+                textBlock.addClass('past');
+            }
 
-    // calling the function to load 
-    window.onload() = savingInput;
-    
+            // checking if hour number is equal to our referance var
+            // removing the past class and adding the present class
+            if (hourNumber === rowTimeRef) {
+                textBlock.removeClass('past');
+                textBlock.addClass('present');
+            }
+
+            // checking if hour number is greater than our referance var
+            // removing the other styles and adding the future style
+            if (hourNumber > rowTimeRef) {
+                textBlock.removeClass('past');
+                textBlock.removeClass('present');
+                textBlock.addClass('future');
+            }
+        })
+    };
+
+    displayDate();
+    timeChecker();
+
+
+
+    // saving to local storage function + keeping input on page
+    // for each loop like above
+    // update save button to select class
+    // get text input same as button
+    // select the one that matches
+
+    saveBtn.on('click', function savingInput() {
+        // added form-control class to the save button in index
+
+        // selecting the class on the input tag
+        var inputs = $(".form-control");
+
+        // added the same time id to the button and the form, but not the button div
+
+        // doing a for each loop
+        inputs.each(function () {
+            var value = $(this).val();
+            var time = $(this).attr('id');
+            localStorage.setItem(time, value);
+
+        })
+
+    });
+
+    function getInput() {
+        // selecting the class on the input tag
+        var inputs = $(".form-control");
+
+        // added the same time id to the button and the form, but not the button div
+
+        // doing a for each loop
+        inputs.each(function () {
+            var time = $(this).attr('id');
+            // getting input for this time
+            var value = localStorage.getItem(time);
+
+            // set value of input field at this time
+
+
+
+        })
+
+    }
+    getInput();
+
 });
-});
 
-savingInput();
+
+
+
+
+
 
